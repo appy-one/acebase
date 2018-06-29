53f9c56512058a17081aaed738c282c24c143b45
appy-one <26569719+appy-one@users.noreply.github.com>
2018-06-29 09:49:29 +0200

    AceBase client code upload

---
+++ acebase-client/src/index.js
@@ -0,0 +1,12 @@
+const { DataReference, DataSnapshot, EventSubscription, PathReference, TypeMappings, TypeMappingOptions } = require('acebase');
+const { AceBaseClient } = require('./acebase-client');
+
+module.exports = { 
+    AceBaseClient,
+    DataReference, 
+    DataSnapshot, 
+    EventSubscription, 
+    PathReference, 
+    TypeMappings, 
+    TypeMappingOptions 
+};
