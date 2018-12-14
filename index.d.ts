502c99c20cd83d1e760752ca62b12c4a15f74850
Ewout Stortenbeker <me@appy.one>
2018-12-14 17:34:13 +0100

    Added types

---
+++ index.d.ts
@@ -0,0 +1,39 @@
+/// <reference types="node" />
+
+// import { EventEmitter } from 'events';
+import * as acebasecore from 'acebase-core';
+
+declare namespace acebase {
+    // extends EventEmitter
+    class AceBase extends acebasecore.AceBaseBase {
+        /**
+         * 
+         * @param {string} dbname | Name of the database to open or create
+         * @param {AceBaseLocalSettings} options | 
+         */
+        constructor(dbname: string, options?: AceBaseLocalSettings);
+
+        /**
+         * Waits for the database to be ready before running your callback. Do this before performing any other actions on your database
+         * @param {()=>void} [callback] (optional) callback function that is called when ready. You can also use the returned promise
+         * @returns {Promise<void>} returns a promise that resolves when ready
+         */
+        ready(callback?: () => void): Promise<void>;
+    }
+
+    interface AceBaseLocalSettings {
+        logLevel?: string;
+        storage?: StorageOptions;
+    }
+
+    interface StorageOptions {
+        recordSize?: number;
+        pageSize?: number;
+        maxInlineValueSize?: number;
+        removeVoidProperties?: boolean;
+        path?:string;
+    }
+
+}
+
+export = acebase;
