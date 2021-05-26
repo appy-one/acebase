3d663f08b624292d57450468cf63e74ddd01f8c6
Ewout Stortenbeker <4ewout@gmail.com>
2021-05-26 16:26:15 +0200

    Added BPlusTree & BinaryBPlusTree unit tests

---
+++ spec/test.js
@@ -0,0 +1,36 @@
+ // To run the unit tests, from the root of the project:
+ //
+ // - install jasmine:
+ //     -> npm i jasmine -g
+ //     -> jasmine (or: npm run test)
+ //
+ // - Or, install jasmine locally:
+ //     -> npm i jasmine --save-dev
+ //     -> npx jasmine
+
+ // To debug the unit tests: 
+ //     -> npm i jasmine --save-dev
+ //     -> launch debugger with this file
+
+const Jasmine = require('jasmine');
+const jasmine = new Jasmine();
+jasmine.loadConfigFile('spec/support/jasmine.json'); // Default
+jasmine.onComplete(function(passed) {
+    if(passed) {
+        console.log('All specs have passed');
+    }
+    else {
+        console.log('At least one spec has failed');
+    }
+});
+jasmine.configureDefaultReporter({
+    // The `timer` passed to the reporter will determine the mechanism for seeing how long the suite takes to run.
+    timer: new jasmine.jasmine.Timer(),
+    // The `print` function passed the reporter will be called to print its results.
+    print: function() {
+        console.log(...arguments); //process.stdout.write(arguments);
+    },
+    // `showColors` determines whether or not the reporter should use ANSI color codes.
+    showColors: true
+});
+jasmine.execute();
