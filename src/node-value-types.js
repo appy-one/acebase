36e02b8f2498c62a8a2d47f62192da610de806cd
Ewout Stortenbeker <4ewout@gmail.com>
2022-01-07 17:56:11 +0100

    preparing for new data type..

--- src/node-value-types.js
+++ src/node-value-types.js
@@ -9,9 +9,9 @@ const VALUE_TYPES = {
     STRING: 5,
     // Custom types:
     DATETIME: 6,
-    //ID: 7
+    // DOCUMENT: 7,     // JSON/XML documents that are contained entirely within the stored node
     BINARY: 8,
-    REFERENCE: 9
+    REFERENCE: 9        // Absolute or relative path to other node
 };
 
 function getValueTypeName(valueType) {
@@ -24,6 +24,7 @@ function getValueTypeName(valueType) {
         case VALUE_TYPES.OBJECT: return 'object';
         case VALUE_TYPES.REFERENCE: return 'reference';
         case VALUE_TYPES.STRING: return 'string';
+        // case VALUE_TYPES.DOCUMENT: return 'document';
         default: 'unknown';
     }
 }
@@ -32,6 +33,7 @@ function getNodeValueType(value) {
     if (value instanceof Array) { return VALUE_TYPES.ARRAY; }
     else if (value instanceof PathReference) { return VALUE_TYPES.REFERENCE; }
     else if (value instanceof ArrayBuffer) { return VALUE_TYPES.BINARY; }
+    // TODO else if (value instanceof DataDocument) { return VALUE_TYPES.DOCUMENT; }
     else if (typeof value === 'string') { return VALUE_TYPES.STRING; }
     else if (typeof value === 'object') { return VALUE_TYPES.OBJECT; }
     throw new Error(`Invalid value for standalone node: ${value}`);
