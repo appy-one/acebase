3ecf5d53987008af872caafb201cad4913d81cac
Ewout Stortenbeker <me@appy.one>
2022-07-25 22:41:27 +0200

    Removed stub js files for storage and data-index

--- src/ts/data-index.js
+++
@@ -1,3 +0,0 @@
-// Used for TS compiler only. Because outDir is different, this makes the import from '../data-index' work
-const { DataIndex } = require('../data-index');
-module.exports = { DataIndex };
--- src/ts/storage.js
+++
@@ -1,3 +0,0 @@
-// Used for TS compiler only. Because outDir is different, this makes the import from '../storage' work
-const { Storage, NodeNotFoundError } = require('../storage');
-module.exports = { Storage, NodeNotFoundError };
