108040f19e52b76632631ea8660c88522a60c745
Ewout Stortenbeker <me@appy.one>
2022-10-17 16:58:45 +0200

    ported browser entry point to typescript

--- src/ts/browser.ts
+++ src/ts/browser.ts
@@ -1,15 +1,15 @@
 /**
    ________________________________________________________________________________
-   
-      ___          ______                
-     / _ \         | ___ \               
-    / /_\ \ ___ ___| |_/ / __ _ ___  ___ 
+
+      ___          ______
+     / _ \         | ___ \
+    / /_\ \ ___ ___| |_/ / __ _ ___  ___
     |  _  |/ __/ _ \ ___ \/ _` / __|/ _ \
     | | | | (_|  __/ |_/ / (_| \__ \  __/
     \_| |_/\___\___\____/ \__,_|___/\___|
                         realtime database
 
-   Copyright 2018-2022 by Ewout Stortenbeker (me@appy.one)   
+   Copyright 2018-2022 by Ewout Stortenbeker (me@appy.one)
    Published under MIT license
 
    See docs at https://github.com/appy-one/acebase
@@ -17,29 +17,45 @@
 
 */
 
-const { DataReference, DataSnapshot, EventSubscription, PathReference, TypeMappings, ID, proxyAccess } = require('acebase-core');
-const { AceBaseLocalSettings } = require('./acebase-local');
-const { BrowserAceBase } = require('./acebase-browser');
-const { CustomStorageSettings, CustomStorageTransaction, CustomStorageHelpers } = require('./storage-custom');
+import { DataReference, DataSnapshot, EventSubscription, PathReference, TypeMappings, ID, proxyAccess } from 'acebase-core';
+import { AceBaseLocalSettings } from './acebase-local';
+import { BrowserAceBase } from './acebase-browser';
+import { CustomStorageSettings, CustomStorageTransaction, CustomStorageHelpers } from './storage/custom';
 
 const acebase = {
-    AceBase: BrowserAceBase, 
+    AceBase: BrowserAceBase,
     AceBaseLocalSettings,
-    DataReference, 
-    DataSnapshot, 
-    EventSubscription, 
-    PathReference, 
+    DataReference,
+    DataSnapshot,
+    EventSubscription,
+    PathReference,
     TypeMappings,
     CustomStorageSettings,
     CustomStorageTransaction,
     CustomStorageHelpers,
     ID,
-    proxyAccess
+    proxyAccess,
 };
 
 // Expose classes to window.acebase:
-window.acebase = acebase;
+(window as any).acebase = acebase;
 // Expose BrowserAceBase class as window.AceBase:
-window.AceBase = BrowserAceBase;
+(window as any).AceBase = BrowserAceBase;
+
 // Expose classes for module imports:
-module.exports = acebase;
+export default acebase;
+
+export {
+    BrowserAceBase as AceBase,
+    AceBaseLocalSettings,
+    DataReference,
+    DataSnapshot,
+    EventSubscription,
+    PathReference,
+    TypeMappings,
+    CustomStorageSettings,
+    CustomStorageTransaction,
+    CustomStorageHelpers,
+    ID,
+    proxyAccess,
+};
