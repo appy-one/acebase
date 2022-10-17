cfcb0566e409f0d83e97408d65ae9e0c399a0401
Ewout Stortenbeker <me@appy.one>
2022-10-17 17:07:56 +0200

    ported main entry to typescript

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,46 +1,25 @@
 /**
    ________________________________________________________________________________
-   
-      ___          ______                
-     / _ \         | ___ \               
-    / /_\ \ ___ ___| |_/ / __ _ ___  ___ 
+
+      ___          ______
+     / _ \         | ___ \
+    / /_\ \ ___ ___| |_/ / __ _ ___  ___
     |  _  |/ __/ _ \ ___ \/ _` / __|/ _ \
     | | | | (_|  __/ |_/ / (_| \__ \  __/
     \_| |_/\___\___\____/ \__,_|___/\___|
                         realtime database
-                                     
-   Copyright 2018-2022 by Ewout Stortenbeker (me@appy.one)   
+
+   Copyright 2018-2022 by Ewout Stortenbeker (me@appy.one)
    Published under MIT license
 
    See docs at https://github.com/appy-one/acebase
    ________________________________________________________________________________
-  
-*/
-const { DataReference, DataSnapshot, EventSubscription, PathReference, TypeMappings, ID, proxyAccess, ObjectCollection, PartialArray } = require('acebase-core');
-const { AceBase, AceBaseLocalSettings } = require('./acebase-local');
-const { AceBaseStorageSettings } = require('./storage-acebase');
-const { SQLiteStorageSettings } = require('./storage-sqlite');
-const { MSSQLStorageSettings } = require('./storage-mssql');
-const { CustomStorageTransaction, CustomStorageSettings, CustomStorageHelpers } = require('./storage-custom');
-const { SchemaValidationError } = require('./storage');
 
-module.exports = {
-    AceBase, 
-    AceBaseLocalSettings,
-    DataReference, 
-    DataSnapshot, 
-    EventSubscription, 
-    PathReference, 
-    TypeMappings,
-    AceBaseStorageSettings,
-    SQLiteStorageSettings,
-    MSSQLStorageSettings,
-    CustomStorageTransaction,
-    CustomStorageSettings,
-    CustomStorageHelpers,
-    SchemaValidationError,
-    ID,
-    proxyAccess,
-    ObjectCollection,
-    PartialArray
-};
+*/
+export { DataReference, DataSnapshot, EventSubscription, PathReference, TypeMappings, ID, proxyAccess, ObjectCollection, PartialArray } from 'acebase-core';
+export { AceBase, AceBaseLocalSettings } from './acebase-local';
+export { AceBaseStorageSettings } from './storage/binary';
+export { SQLiteStorageSettings } from './storage/sqlite';
+export { MSSQLStorageSettings } from './storage/mssql';
+export { CustomStorageTransaction, CustomStorageSettings, CustomStorageHelpers } from './storage/custom';
+export { SchemaValidationError } from './storage';
