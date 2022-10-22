5931b53bc220bffc3f9e324d9bea4b9dc7a3f55f
Ewout Stortenbeker <me@appy.one>
2022-10-22 22:35:22 +0200

    improved types, moved localStorage implementation

--- src/ts/acebase-local.ts
+++ src/ts/acebase-local.ts
@@ -1,49 +1,60 @@
-import { AceBaseBase, AceBaseBaseSettings, LoggingLevel } from 'acebase-core';
+import { AceBaseBase, AceBaseBaseSettings } from 'acebase-core';
 import { AceBaseStorage } from './storage/binary';
 import { LocalApi } from './api-local';
 import { IPCClientSettings, StorageSettings, TransactionLogSettings } from './storage';
-import { CustomStorageSettings, CustomStorageTransaction, CustomStorageHelpers, ICustomStorageNode, ICustomStorageNodeMetaData } from './storage/custom';
+import { createLocalStorageInstance, LocalStorageSettings } from './storage/custom/local-storage';
+import { IndexedDBStorageSettings } from './storage/custom/indexed-db/settings';
+
+export { LocalStorageSettings, IndexedDBStorageSettings };
 
 export class AceBaseLocalSettings extends AceBaseBaseSettings {
 
-    storage: Partial<StorageSettings>;
-    ipc: IPCClientSettings;
-    transactions: TransactionLogSettings;
-    info: string;
+    /**
+     * Optional storage settings
+     */
+    storage?: Partial<StorageSettings>;
 
-    constructor(options: Partial<AceBaseLocalSettings>) {
+    /**
+     * IPC settings if you are using AceBase in pm2 or cloud-based clusters
+     * @deprecated Move this to `storage` settings
+     */
+    ipc?: IPCClientSettings;
+
+    /**
+     * Settings for optional transaction logging
+     * @deprecated Move this to `storage` settings
+     */
+    transactions?: TransactionLogSettings;
+
+    constructor(options: Partial<AceBaseLocalSettings> = {}) {
         super(options);
-        if (!options) { options = {}; }
-        this.storage = options.storage || {};
 
-        // Copy IPC and transaction settings to storage settings
-        if (typeof options.ipc === 'object') {
-            this.storage.ipc = options.ipc;
-        }
-        if (typeof options.transactions === 'object') {
-            this.storage.transactions = options.transactions;
+        if (options.storage) {
+            this.storage = options.storage;
+            // If they were set on global settings, copy IPC and transaction settings to storage settings
+            if (options.ipc) { this.storage.ipc = options.ipc; }
+            if (options.transactions) { this.storage.transactions = options.transactions; }
         }
     }
 }
 
+
 export class AceBase extends AceBaseBase {
 
     /**
-     * @internal should not be accessed from external modules
+     * @internal (for internal use)
      */
     public api: LocalApi;
 
     /**
      * @param dbname Name of the database to open or create
      */
-    constructor(dbname: string, options: Partial<AceBaseLocalSettings>) {
-        options = new AceBaseLocalSettings(options);
-        options.info = options.info || 'realtime database';
-        super(dbname, options);
+    constructor(dbname: string, init: Partial<AceBaseLocalSettings> = {}) {
+        const settings = new AceBaseLocalSettings(init);
+        super(dbname, settings);
         const apiSettings = {
             db: this as AceBaseBase,
-            storage: options.storage as StorageSettings,
-            logLevel: options.logLevel,
+            settings,
         };
         this.api = new LocalApi(dbname, apiSettings, () => {
             this.emit('ready');
@@ -85,165 +96,17 @@ export class AceBase extends AceBaseBase {
      * @param dbname Name of the database
      * @param settings optional settings
      */
-    static WithLocalStorage(dbname: string, settings: Partial<{
-        /**
-         * what level to use for logging to the console
-         */
-        logLevel: LoggingLevel;
-        /**
-         * whether to use sessionStorage instead of localStorage
-         */
-        temp: boolean;
-        /**
-         * Alternate localStorage provider for running in non-browser environments. Eg using 'node-localstorage'
-         */
-        provider: any;
-        /**
-         * Whether to remove undefined property values of objects being stored, instead of throwing an error
-         * @default false
-         */
-        removeVoidProperties: boolean;
-        /**
-         * Maximum size of binary data/strings to store in parent object records. Larger values are stored in their own records.
-         * Recommended to keep this at the default setting
-         * @default 50
-         */
-        maxInlineValueSize: number;
-        /**
-         * Whether to enable cross-tab synchronization
-         * @default false
-         */
-        multipleTabs: boolean;
-        /**
-         * You can turn this on if you are a sponsor
-         * @default false
-         */
-        sponsor: boolean;
-    }>) {
-
-        settings = settings || {};
-        if (!settings.logLevel) { settings.logLevel = 'error'; }
-
-        // Determine whether to use localStorage or sessionStorage
-        const localStorage = settings.provider ? settings.provider : settings.temp ? window.localStorage : window.sessionStorage;
-
-        // Setup our CustomStorageSettings
-        const storageSettings = new CustomStorageSettings({
-            name: 'LocalStorage',
-            locking: true,
-            removeVoidProperties: settings.removeVoidProperties,
-            maxInlineValueSize: settings.maxInlineValueSize,
-            ready() {
-                // LocalStorage is always ready
-                return Promise.resolve();
-            },
-            getTransaction(target: { path: string; write: boolean }) {
-                // Create an instance of our transaction class
-                const context = {
-                    debug: true,
-                    dbname,
-                    localStorage,
-                };
-                const transaction = new LocalStorageTransaction(context, target);
-                return Promise.resolve(transaction);
-            },
-        });
-        const db = new AceBase(dbname, { logLevel: settings.logLevel, storage: storageSettings, sponsor: settings.sponsor });
-        db.settings.ipcEvents = settings.multipleTabs === true;
-
+    static WithLocalStorage(dbname: string, settings: Partial<LocalStorageSettings> = {}) {
+        const db = createLocalStorageInstance(dbname, settings);
         return db;
     }
 
-}
-
-// Setup CustomStorageTransaction for browser's LocalStorage
-class LocalStorageTransaction extends CustomStorageTransaction {
-
-    private _storageKeysPrefix: string;
-
-    constructor(public context: {debug: boolean, dbname: string, localStorage: typeof window.localStorage}, target: {path: string, write: boolean}) {
-        super(target);
-        this._storageKeysPrefix = `${this.context.dbname}.acebase::`;
-    }
-
-    async commit() {
-        // All changes have already been committed. TODO: use same approach as IndexedDB
-    }
-
-    async rollback(err: any) {
-        // Not able to rollback changes, because we did not keep track
-    }
-
-    async get(path: string) {
-        // Gets value from localStorage, wrapped in Promise
-        const json = this.context.localStorage.getItem(this.getStorageKeyForPath(path));
-        const val = JSON.parse(json);
-        return val;
-    }
-
-    async set(path: string, val: any) {
-        // Sets value in localStorage, wrapped in Promise
-        const json = JSON.stringify(val);
-        this.context.localStorage.setItem(this.getStorageKeyForPath(path), json);
-    }
-
-    async remove(path: string) {
-        // Removes a value from localStorage, wrapped in Promise
-        this.context.localStorage.removeItem(this.getStorageKeyForPath(path));
-    }
-
-    async childrenOf(path: string, include: { metadata?: boolean; value?: boolean }, checkCallback: (path: string) => boolean, addCallback: (path: string, node: ICustomStorageNodeMetaData|ICustomStorageNode) => boolean) {
-        // Streams all child paths
-        // Cannot query localStorage, so loop through all stored keys to find children
-        const pathInfo = CustomStorageHelpers.PathInfo.get(path);
-        for (let i = 0; i < this.context.localStorage.length; i++) {
-            const key = this.context.localStorage.key(i);
-            if (!key.startsWith(this._storageKeysPrefix)) { continue; }
-            const otherPath = this.getPathFromStorageKey(key);
-            if (pathInfo.isParentOf(otherPath) && checkCallback(otherPath)) {
-                let node;
-                if (include.metadata || include.value) {
-                    const json = this.context.localStorage.getItem(key);
-                    node = JSON.parse(json);
-                }
-                const keepGoing = addCallback(otherPath, node);
-                if (!keepGoing) { break; }
-            }
-        }
-    }
-
-    async descendantsOf(path: string, include: { metadata?: boolean; value?: boolean }, checkCallback: (path: string) => boolean, addCallback: (path: string, node: ICustomStorageNodeMetaData|ICustomStorageNode) => boolean) {
-        // Streams all descendant paths
-        // Cannot query localStorage, so loop through all stored keys to find descendants
-        const pathInfo = CustomStorageHelpers.PathInfo.get(path);
-        for (let i = 0; i < this.context.localStorage.length; i++) {
-            const key = this.context.localStorage.key(i);
-            if (!key.startsWith(this._storageKeysPrefix)) { continue; }
-            const otherPath = this.getPathFromStorageKey(key);
-            if (pathInfo.isAncestorOf(otherPath) && checkCallback(otherPath)) {
-                let node;
-                if (include.metadata || include.value) {
-                    const json = this.context.localStorage.getItem(key);
-                    node = JSON.parse(json);
-                }
-                const keepGoing = addCallback(otherPath, node);
-                if (!keepGoing) { break; }
-            }
-        }
-    }
-
     /**
-     * Helper function to get the path from a localStorage key
+     * Creates an AceBase database instance using IndexedDB as storage engine. Only available in browser contexts!
+     * @param dbname Name of the database
+     * @param settings optional settings
      */
-    getPathFromStorageKey(key: string) {
-        return key.slice(this._storageKeysPrefix.length);
-    }
-
-    /**
-     * Helper function to get the localStorage key for a path
-     */
-    getStorageKeyForPath(path: string) {
-        return `${this._storageKeysPrefix}${path}`;
+    static WithIndexedDB(dbname: string, init: Partial<IndexedDBStorageSettings> = {}): AceBase {
+        throw new Error(`IndexedDB storage can only be used in browser contexts`);
     }
 }
-
