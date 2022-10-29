fef5dfcc10dfa68de5b8fd41f4c095e4eac900e2
Ewout Stortenbeker <me@appy.one>
2022-10-29 11:51:11 +0200

    remove unused promise.try shim

--- src/promise-try-shim.js
+++
@@ -1,9 +0,0 @@
-// Polyfill for Promise.try, promisifies synchronous code,
-// resolving with function's return value and catching synchronous errors with a promise rejection
-if (!Promise.try) {
-    Promise.try = function(fn) {
-        return new Promise(resolve => {
-            resolve(fn());
-        });
-    };
-}
