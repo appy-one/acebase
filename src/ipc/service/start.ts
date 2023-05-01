29cbb253c7a97dc203cc0f0ba5be507151757198
Ewout Stortenbeker <me@appy.one>
2023-05-01 17:19:25 +0200

    Add command level arguments

--- src/ipc/service/start.ts
+++ src/ipc/service/start.ts
@@ -1,10 +1,23 @@
 import { startServer } from '.';
+import { type AceBaseLocalSettings } from '../../';
 
 (async () => {
     try {
         const dbFile = process.argv[2]; // full path to db storage file, eg '/home/ewout/project/db.acebase/data.db'
-        await startServer(dbFile, (code) => {
-            process.exit(code);
+        const settings = process.argv.slice(3).reduce((settings, arg, i, args) => {
+            switch (arg.toLowerCase()) {
+                case '--loglevel': settings.logLevel = args[i + 1] as AceBaseLocalSettings['logLevel']; break;
+                case '--maxidletime': settings.maxIdleTime = parseInt(args[i + 1]); break;
+            }
+            return settings;
+        }, { logLevel: 'log' as AceBaseLocalSettings['logLevel'], maxIdleTime: 5000 });
+
+        await startServer(dbFile, {
+            logLevel: settings.logLevel,
+            maxIdleTime: settings.maxIdleTime,
+            exit: (code) => {
+                process.exit(code);
+            },
         });
     }
     catch (err) {
