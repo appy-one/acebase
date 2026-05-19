1ef60e25cfa840d48e2d589b57ceda4fd9a20652
Ewout Stortenbeker <me@appy.one>
2026-05-19 12:08:24 +0200

    Win32: change named pipe to use a similar name as a unix socket

--- src/ipc/service/shared.ts
+++ src/ipc/service/shared.ts
@@ -1,20 +1,17 @@
 import { createHash } from 'crypto';
 
 export function getSocketPath(filePath: string) {
-    if (process.platform === 'win32') {
-        let path = `\\\\.\\pipe\\${filePath.replace(/^\//, '').replace(/\//g, '-')}`;
-        if (path.length > 256) {
-            const hash = createHash('sha256').update(path).digest('hex');
-            path = `\\\\.\\pipe\\${hash}`;
-        }
-        return path;
-    }
-    // Create a Unix socket at /tmp/[dbName]-[dbType]-[shortHash].acebase.sock
     const match = filePath.match(/[\\\/]([^\\\/]+)\.acebase[\\\/]([^\\\/]+)\.([^.\\\/]+)$/);
     const dbName = match ? match[1] : 'db';
     const dbType = match ? match[2] : 'data';
     const shortHash = createHash('sha256').update(filePath).digest('hex').slice(0, 8);
-    return `/tmp/${dbName}-${dbType}-${shortHash}.acebase.sock`;
+    if (process.platform === 'win32') {
+        // Create named pipe
+        return `\\\\.\\pipe\\${dbName}-${dbType}-${shortHash}.acebase`;
+    } else {
+        // Create Unix socket at /tmp/[dbName]-[dbType]-[shortHash].acebase.sock
+        return `/tmp/${dbName}-${dbType}-${shortHash}.acebase.sock`;
+    }
 }
 
 // export const MSG_DELIMITER = String.fromCharCode(0) + String.fromCharCode(1) + String.fromCharCode(3) + String.fromCharCode(5) + String.fromCharCode(0);
