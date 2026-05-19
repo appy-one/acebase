26e6b7fdea2520370018ae7fc61b584a890efa64
Ewout Stortenbeker <me@appy.one>
2026-05-19 11:41:56 +0200

    Fix IPC socket on Mac/Linux, improve startup/exit flow

--- src/ipc/service/shared.ts
+++ src/ipc/service/shared.ts
@@ -1,18 +1,20 @@
 import { createHash } from 'crypto';
 
 export function getSocketPath(filePath: string) {
-    let path = process.platform ==='win32'
-        ? `\\\\.\\pipe\\${filePath.replace(/^\//, '').replace(/\//g, '-')}`
-        : `${filePath}.sock`;
-    const maxLength = process.platform ==='win32' ? 256 : 108;
-    if (path.length > maxLength) {
-        // Use hash of filepath instead
-        const hash = createHash('sha256').update(path).digest('hex');
-        path = process.platform ==='win32'
-            ? `\\\\.\\pipe\\${hash}`
-            : `${hash}.sock`;
+    if (process.platform === 'win32') {
+        let path = `\\\\.\\pipe\\${filePath.replace(/^\//, '').replace(/\//g, '-')}`;
+        if (path.length > 256) {
+            const hash = createHash('sha256').update(path).digest('hex');
+            path = `\\\\.\\pipe\\${hash}`;
+        }
+        return path;
     }
-    return path;
+    // Create a Unix socket at /tmp/[dbName]-[dbType]-[shortHash].acebase.sock
+    const match = filePath.match(/[\\\/]([^\\\/]+)\.acebase[\\\/]([^\\\/]+)\.([^.\\\/]+)$/);
+    const dbName = match ? match[1] : 'db';
+    const dbType = match ? match[2] : 'data';
+    const shortHash = createHash('sha256').update(filePath).digest('hex').slice(0, 8);
+    return `/tmp/${dbName}-${dbType}-${shortHash}.acebase.sock`;
 }
 
 // export const MSG_DELIMITER = String.fromCharCode(0) + String.fromCharCode(1) + String.fromCharCode(3) + String.fromCharCode(5) + String.fromCharCode(0);
